224e304bcbc9f139916b15bdc0722d7ef49351cb
J Smith <jay@zoocasa.com>
2009-08-19 15:36:13 +0000

    Move stuff into a module for safety.
    git-svn-id: file:///usr/local/svnroot/ruby_extensions/gmaps_tools/trunk@14997 05ac5844-ee52-4e45-b639-0480b35ff74b

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 
+require File.join(File.dirname(__FILE__), 'lib', 'gmaps_tools')
 require File.join(File.dirname(__FILE__), 'lib', 'polyline_encoder')
 require File.join(File.dirname(__FILE__), 'lib', 'google_projection')
 
---
+++ lib/gmaps_tools.rb
@@ -0,0 +1,4 @@
+
+module GoogleMaps
+end
+
