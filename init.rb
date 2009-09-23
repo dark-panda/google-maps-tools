bbd7cee851df6e2b7efb61dbb4d8aa86d4931b28
J Smith <jay@zoocasa.com>
2009-09-23 22:23:13 +0000

    Rename file.
    git-svn-id: file:///usr/local/svnroot/ruby_extensions/gmaps_tools/trunk@15611 05ac5844-ee52-4e45-b639-0480b35ff74b

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 
 require File.join(File.dirname(__FILE__), 'lib', 'gmaps_tools')
 require File.join(File.dirname(__FILE__), 'lib', 'polyline_encoder')
-require File.join(File.dirname(__FILE__), 'lib', 'google_projection')
+require File.join(File.dirname(__FILE__), 'lib', 'projection')
 
