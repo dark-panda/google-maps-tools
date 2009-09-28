154e074fe08c64eb3a0286a66c60bbe8068b7bf8
J Smith <jay@zoocasa.com>
2009-09-28 20:11:22 +0000

    Constants for Google Maps.
    git-svn-id: file:///usr/local/svnroot/ruby_extensions/gmaps_tools/trunk@15752 05ac5844-ee52-4e45-b639-0480b35ff74b

---
+++ lib/constants.rb
@@ -0,0 +1,49 @@
+
+module GoogleMaps
+	module Constants
+		# http://code.google.com/apis/maps/documentation/reference.html#GGeoAddressAccuracy
+		GEO_ADDRESS_ACCURACY = {
+			# (Since 2.59)
+			:unknown_location => 0,
+
+			# Country level accuracy.
+			:country => 1,
+
+			# Region (state, province, prefecture, etc.) level accuracy.
+			:region => 2,
+
+			# Sub-region (county, municipality, etc.) level accuracy.
+			:sub_region => 3,
+
+			# Town (city, village) level accuracy.
+			:city => 4,
+
+			# Post code (zip code) level accuracy.
+			:postal_code => 5,
+
+			# Street level accuracy.
+			:street_level => 6,
+
+			# Intersection level accuracy.
+			:intersection => 7,
+
+			# Address level accuracy.
+			:address => 8,
+
+			# Premise (building name, property name, shopping center, etc.)
+			# level accuracy.
+			:premise => 9
+		}.freeze
+
+		# http://code.google.com/apis/maps/documentation/geocoding/#GeocodingResponses
+		GEO_STATUS_CODES = {
+			:success => 200,
+			:server_error => 500,
+			:missing_query => 601,
+			:unknown_address => 602,
+			:unavailable_address => 603,
+			:bad_key => 610,
+			:too_many_queries => 620
+		}.freeze
+	end
+end
